06a131e0a0651682f0d87d3cb6e9426649b529f2
Wombabonga <Lukasepson@ish.de>
2015-02-19 20:59:42 +0100

    Bild feld Hinzugeügt

--- src/SQL/wsm.sql
+++ src/SQL/wsm.sql
@@ -7,6 +7,7 @@ CREATE TABLE IF NOT EXISTS 'Produkt' (
     'ProduktID' int(11) NOT NULL AUTO_INCREMENT,
     'Produktname' varchar(20) NOT NULL,
     'ProduktBeschreibung' text,
+    'Bild' varchar(20) DEFAULT NULL,
     'Kategorie' varchar(20) DEFAULT NULL,
     'LagerBestand' int(4) DEFAULT NULL,
     'Preis' float(6,2) DEFAULT NULL,
