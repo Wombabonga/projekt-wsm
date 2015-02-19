260ee2f025280dd27fe2f0ea7fc8dc2a3a15e3e6
Wombabonga <Lukasepson@ish.de>
2015-02-19 21:40:53 +0100

    Grundgerüst

--- src/header.php
+++ src/header.php
@@ -4,9 +4,19 @@
     
         <meta charset="utf-8"/>
         
+       <link rel="stylesheet" type="text/css" href="css/Layout.css">
+        
         <title>WSM - ProjektName</title>
     
     
     </head>
 
     <body>
+        
+  
+        
+            <div id="header">
+                
+                <h1>MWS</h1>
+        
+            </div>
--- src/index.php
+++ src/index.php
@@ -3,12 +3,11 @@
 include 'header.php';
 include 'Connector.php';
 
-$db = NEW DBconnector();
-$db->test();
-
 ?>
 
-    Hello world
+
+Hello world
+
 
 
 <?php include 'footer.php' ?>
