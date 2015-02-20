a52a09abf00c26e887087dc60e161f0349437634
Wombabonga <Lukasepson@ish.de>
2015-02-20 17:13:18 +0100

    in unterschiedliche div s eingeteilt

--- src/index.php
+++ src/index.php
@@ -1,13 +1,25 @@
+
+<div id="headerbackground">
 <?php
 
 include 'header.php';
-include 'Connector.php';
-
 ?>
 
+</div>
 
-Hello world
+<div id="contentbackground">
+    <div id="content">
+    
+        
+    
+            Hello<br> <br> this<br> is<br> a<br> test<br> Message
+    
+    </div>
+    
+</div>
 
 
 
+<div id="footerbackground">
 <?php include 'footer.php' ?>
+</div>
