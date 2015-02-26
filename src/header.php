6db8dac4204926c11c1ae3741591725d4ff279fe
Wombabonga <Lukasepson@ish.de>
2015-02-26 20:29:09 +0100

    navigations leiste hinzugefügt

--- src/header.php
+++ src/header.php
@@ -8,26 +8,72 @@
         
         <title>WSM - ProjektName</title>
         
-        <?php include 'Connector.php'; include 'Util.php'; ?>
+        <?php include 'Connector.php'; include 'ViewBuilder.php'; ?>
     
     </head>
 
     <body>
- 
+
         <div id="imgLogo">
-        
+
             <img src="img/background/minecraft_logo.png">
-                
+
         </div>
-        
+
         <div class="headerbackground">
-        
-            <p class="header">
 
-                Hello 
+            <div class="header">
 
-            </p>
+                <div id="navigation">
 
-        
+                    <form action="index.php" method="post">
+
+                        <ul>
+
+                            <?php 
+
+                            $allKat = mysql_query('SELECT * FROM Kategorie');
+                            while($kat = mysql_fetch_array($allKat, MYSQL_ASSOC)){
+                                echo '<li>';
+
+                                echo '<a href="index.php?kategorie=' . $kat['KategorieID'] . '">';
+                                echo $kat['KategorieName'];
+                                echo '</a>';
+
+                                echo '</li>';
+
+
+
+                            }
+
+
+                            ?>
+
+                            <li>
+                            
+                                Suche 
+                            
+                            </li>
+                            
+                            <li>
+
+                                <input type="text" value="suche">
+
+                            </li>
+                            
+                            <li>
+                                
+                                <input type="submit" name="Allonsy" value="LOS">
+                            
+                            </li>
+
+
+                        </ul>
+
+                    </form>
+
+                </div>
+
+            </div>
+
         </div>
-        
