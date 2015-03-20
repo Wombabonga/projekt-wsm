9023e95d4f07efbe4b7780292bfd17c9b0334bd1
Wombabonga-linux <Lukasepson@ish.de>
2015-03-20 20:07:46 +0100

    Zeigt nun alle Bestellungen von einem Kunden an

--- src/Backend/backshowbestellungen.php
+++ src/Backend/backshowbestellungen.php
@@ -5,27 +5,27 @@ include 'backheader.php';
 $id = $_GET['kundeid'];
 
 $result = $mysql->query("
-SELECT Bestellungen.BestellID AS BestellID, Bestelldetails.Anzahl AS Anzahl
-FROM Kunde
-INNER JOIN Bestellungen (
-    INNER JOIN Bestelldetails ON Bestelldetails.BestellID = Bestellungen.BestellID
-)ON Bestellungen.KundeID = Kunde.KundeID
-WHERE Kunde.KundeID = " . $id . ";"); 
-
-
-/*$result = $mysql->query('
-SELECT Bestellungen.BestellID AS BestellID, Bestellungen.Datum AS Datum
-FROM Kunde
-INNER JOIN Bestellungen ON Bestellungen.KundeID = Kunde.KundeID
-WHERE Kunde.KundeID = ' . $id . ';'); 
-*/
-
-if($result) echo "DONE <br>";
-else echo $mysql->error;
-
+SELECT
+    Bestellungen.BestellID AS BestellID, Produkt.ProduktName AS ProduktName, Bestelldetails.Anzahl AS Anzahl, Bestellungen.Datum AS Datum
+FROM
+    Bestellungen
+INNER JOIN
+    Bestelldetails
+ON
+    Bestelldetails.BestellID = Bestellungen.BestellID
+INNER JOIN
+    Kunde
+ON
+    Bestellungen.KundeID = Kunde.KundeID
+INNER JOIN
+    Produkt
+ON
+    Produkt.ProduktID = Bestelldetails.ProduktID
+WHERE 
+    Bestellungen.KundeID = " . $id. ";");
 
 while($bestellung = $result->fetch_object()){
-    echo $bestellung->BestellID . " - " . $bestellung->Anzahl . '<br>';
+    echo $bestellung->BestellID . " - " . $bestellung->ProduktName . " - " . $bestellung->Anzahl . " - " . $bestellung->Datum . '<br>';
 }
 
 
