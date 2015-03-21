9d23dd9a29fad31616f960b8799067ab0f90494a
Wombabonga <Lukasepson@ish.de>
2015-03-21 16:34:50 +0100

    Impressum Hinzugefügt

---
+++ src/impressum.php
@@ -0,0 +1,44 @@
+<html>
+    
+    <style>
+    
+        html{
+            background-color: rgba(244, 231, 160, 0.85);
+            font-weight: 800;
+        }
+    
+    </style>
+    
+    <head>
+        <meta charset="utf-8"/>
+        
+        <title>Impressum</title>
+    
+    </head>
+
+    <body>
+    
+        
+        <h1>Impressum</h1>
+    
+        <p>Entwickler</p>
+        
+        <ul>
+        
+            <li>Lukas Hülshorst</li>
+        
+            <li>Alexander Peters</li>
+        
+        </ul>
+        
+        <p>Benutzte Bilder</p>
+        
+        <ul>
+            
+            <li><a href="http://bdcraft.net/purebdcraft-minecraft">SPHAX PUREBDCRAFT Resources Pack for Minecraft </a></li>
+        
+        </ul>
+        
+    </body>
+
+</html>
